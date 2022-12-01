02796d615424c5e7087464f5f7133bab2fc2e349
cw417 <chriswhitlock417@gmail.com>
2022-11-30 20:47:09 -0500

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Lab 10c</div>
+      <div>Hey, Nikita - I win :)</div>
+    </div>
+  );
+}
+
+export default App;
